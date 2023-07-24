d18bd9f46df3332c72e11c1a8e9a7c9e36d8b4d4
MINH YEN <minhyen1311414@gmail.com>
2023-07-24 22:05:14 +0700

    Create DriverManager and Update ListElement.

--- Sel3_Automation/src/test/java/testcases/BaseTest.java
+++ Sel3_Automation/src/test/java/testcases/BaseTest.java
@@ -37,7 +37,7 @@ public class BaseTest {
         Driver.initWebDriver(String.valueOf(Thread.currentThread().getId()), BROWSER_1, property);
         Driver.maximize();
 
-//        Driver.navigateTo(Constant.URL);
+////        Driver.navigateTo(Constant.URL);
 
 
     }
