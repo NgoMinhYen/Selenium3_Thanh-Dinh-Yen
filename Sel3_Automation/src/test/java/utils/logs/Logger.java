ba33492d1df0634fbffe9344b9eee8c88885c670
duyenthanh412 <duyenthanh412@gmail.com>
2023-07-27 21:00:48 +0700

    Update log and extend report

--- Sel3_Automation/src/test/java/utils/logs/Logger.java
+++ Sel3_Automation/src/test/java/utils/logs/Logger.java
@@ -1,19 +1,8 @@
 package utils.logs;
 
-import core.framework.wrappers.Driver;
-import io.qameta.allure.model.Status;
-import io.qameta.allure.model.StepResult;
 import org.apache.logging.log4j.LogManager;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import utils.extentreports.ExtentTestManager;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.UUID;
-
-import static io.qameta.allure.Allure.getLifecycle;
+import com.aventstack.extentreports.Status;
 
 /**
  * Custom logger
@@ -25,18 +14,6 @@ public class Logger {
     public void step(String message) {
         logger.info(message);
         ExtentTestManager.logMessage(message);
-        final String uuid = UUID.randomUUID().toString();
-        getLifecycle().startStep(uuid, new StepResult().setName(message).setStatus(Status.PASSED));
-
-        try {
-            File src = ((TakesScreenshot) Driver.getWebDriver()).getScreenshotAs(OutputType.FILE);
-            getLifecycle().addAttachment(uuid, "image/png", ".png", Files.readAllBytes(src.toPath()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        getLifecycle().stopStep(uuid);
-
     }
 
     public void info(String message) {
@@ -45,18 +22,22 @@ public class Logger {
     }
 
     public void warn(String message) {
+        ExtentTestManager.logMessage(Status.WARNING,message);
         logger.warn(message);
     }
 
     public void error(String message) {
+        ExtentTestManager.logMessage(Status.FAIL, message);
         logger.error(message);
     }
 
     public void fatal(String message) {
+        ExtentTestManager.logMessage(message);
         logger.fatal(message);
     }
 
     public void debug(String message) {
+        ExtentTestManager.logMessage(message);
         logger.debug(message);
     }
 
