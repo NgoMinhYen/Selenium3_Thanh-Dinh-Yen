36686a727c3af1088f0df60db4cf1fad09e3f46c
dinhuynh21 <dinhhuynh21@gmail.com>
2023-07-31 22:50:30 +0700

    Fixed conflict

--- Sel3_Automation/src/test/java/testcases/BaseTest.java
+++ Sel3_Automation/src/test/java/testcases/BaseTest.java
@@ -37,7 +37,7 @@ public class BaseTest {
         Driver.initWebDriver(String.valueOf(Thread.currentThread().getId()), BROWSER_1, property);
         Driver.maximize();
 
-        Driver.navigateTo(Constant.URL_DIGIKEY);
+        Driver.navigateTo(Constant.URL);
     }
 
 //    public void openBrowser() {
