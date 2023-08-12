1627ac832a99044c463a5cde8f61d2c1307241d1
dinhuynh21 <dinhhuynh21@gmail.com>
2023-08-12 12:11:05 +0700

    Add Account page method

---
+++ Sel3_Automation/src/test/java/dataobjects/Admin.java
@@ -0,0 +1,77 @@
+package dataobjects;
+
+import utils.common.Utilities;
+import utils.common.constants.Constant;
+
+public class Admin {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String phone;
+    private String profile;
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public Admin() {}
+
+    public Admin(String firstName, String lastName, String userName, String phone,
+                  String profile) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.phone = phone;
+        this.profile = profile;
+    }
+
+    public static Admin generateRandomAdmin() {
+        Admin admin = new Admin();
+        admin.setFirstName("First" + Utilities.generateString(10));
+        admin.setLastName("Last" + Utilities.generateString(10));
+        admin.setUserName(Utilities.generateString(8) + Constant.EMAIL_TAIL);
+        admin.setPhone("0" + Utilities.generateNumber(9));
+        return admin;
+    }
+    public Admin generateRandomAdmin(String profile) {
+        Admin admin = generateRandomAdmin();
+        admin.setProfile(profile);
+        return admin;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- Sel3_Automation/src/test/java/pageobjects/AccountsPage.java
+++ Sel3_Automation/src/test/java/pageobjects/AccountsPage.java
@@ -4,7 +4,9 @@ import core.framework.elements.IElement;
 import core.framework.source.Find;
 import core.framework.source.Page;
 import core.framework.source.ResourcePage;
+import dataobjects.Admin;
 import org.openqa.selenium.support.ui.Select;
+import utils.enums.LeftMenu;
 
 @ResourcePage(file = "accountsPage.properties")
 public class AccountsPage extends AbstractPage {
@@ -15,8 +17,36 @@ public class AccountsPage extends AbstractPage {
     @Find(key = "eleSelectRole")
     private IElement selectOptionRole;
 
+    @Find(key = "eleAdminNameList")
+    private IElement eleAdminNameList;
+    @Find(key = "eleAdminUserNameList")
+    private IElement eleAdminUserNameList;
+    @Find(key = "eleAdminPhoneList")
+    private IElement eleAdminPhoneList;
+
+    @Find(key = "btnNextPage")
+    private IElement btnNextPage;
+
+    /// Section Invite Popup
+    @Find(key = "invitePopUp")
+    private IElement invitePopUp;
+    @Find(key = "txtFirstNamePopUp")
+    private IElement txtFirstNamePopUp;
+    @Find(key = "txtLastNamePopUp")
+    private IElement txtLastNamePopUp;
+    @Find(key = "txtUserNamePopUp")
+    private IElement txtUserNamePopUp;
+    @Find(key = "txtPhoneNamePopUp")
+    private IElement txtPhoneNamePopUp;
+
     @Find(key = "eleUploadFile")
     private IElement btnUploadFile;
+    @Find(key = "btnSave")
+    private IElement btnSave;
+    @Find(key = "btnCancel")
+    private IElement btnCancel;
+    ///
+
 
     private AccountsPage() {
         Page.init(this);
@@ -34,7 +64,22 @@ public class AccountsPage extends AbstractPage {
 
     }
     public AccountsPage clickInviteUser(){
+        inviteUser.waitForVisibility();
         inviteUser.click();
+        return AccountsPage.getInstance();
+    }
+    public void waitForInvitePopupDisplayed(){
+        invitePopUp.waitForVisibility();
+    }
+    public AccountsPage fillInvitePopup(Admin admin){
+        txtPhoneNamePopUp.waitForVisibility();
+        txtFirstNamePopUp.enter(admin.getFirstName());
+        txtLastNamePopUp.enter(admin.getLastName());
+        txtUserNamePopUp.enter(admin.getUserName());
+        txtPhoneNamePopUp.enter(admin.getPhone());
+        if(admin.getProfile() != null){
+            uploadProfile(admin.getProfile());
+        }
         return AccountsPage.getInstance();
     }
     public AccountsPage uploadProfile(String path){
@@ -43,6 +88,35 @@ public class AccountsPage extends AbstractPage {
         btnUploadFile.enter(path);
         return AccountsPage.getInstance();
     }
-
-
+    public boolean isSaveButtonEnable(){
+        btnSave.waitForVisibility();
+        return btnSave.isEnabled();
+    }
+    public AccountsPage clickSave(){
+        btnSave.waitForClickable();
+        btnSave.click();
+        return AccountsPage.getInstance();
+    }
+    public AccountsPage clickCancel(){
+        btnCancel.waitForClickable();
+        btnCancel.click();
+        return AccountsPage.getInstance();
+    }
+    public boolean isAdminDetailDisplayed(Admin admin){
+        boolean result;
+        do {
+            result = checkAdminDetailDisplayed(admin);
+            if(btnNextPage.isEnabled()) btnNextPage.click();
+        }
+        while (btnNextPage.isEnabled());
+        return result;
+    }
+    public boolean checkAdminDetailDisplayed(Admin admin){
+        boolean result = true;
+        result = result && eleAdminNameList.of(admin.getFirstName() + " " + admin.getLastName())
+                .isDisplayed();
+        result = result && eleAdminUserNameList.of(admin.getUserName()).isDisplayed();
+        result = result && eleAdminPhoneList.of(admin.getPhone()).isDisplayed();
+        return result;
+    }
 }
