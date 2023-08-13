9e28db1b11ad5f5748553072231bf8dbe5815e27
duyenthanh412 <duyenthanh412@gmail.com>
2023-08-13 13:43:35 +0700

    Update test cases

--- Sel3_Automation/src/test/java/testcases/HomePageTest.java
+++ Sel3_Automation/src/test/java/testcases/HomePageTest.java
@@ -21,13 +21,13 @@ public class HomePageTest extends BaseTest {
         homePage = loginPage.login(Constant.USER_ADMIN);
         homePage.waitForPageLoadingComplete();
 
-        logger.step("Step 3. Click Logout");
+        logger.step("Step 3. Click \"Logout\" on the left menu");
         homePage.selectLogout();
 
         logger.step("VP Step 3. \"Do you want to logout?\" popup is displayed");
         Assert.assertTrue(homePage.isDisplayedEleOnPopup(Message.DO_YOU_WANT_TO_LOGOUT.getValue()), "\"Do you want to logout?\" popup is displayed");
 
-        logger.step("Step 4. Click Yes on popup");
+        logger.step("Step 4. Click \"Yes\" on popup");
         homePage.clickElement(UserActions.YES.getValue());
 
         logger.step("VP Step 4. Login page is displayed");
